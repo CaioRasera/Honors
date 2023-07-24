df0dd0568d42422c7b628fb3452a6245a7a9626d
CaioRasera <caiocremoninirasera@gmail.com>
2023-07-24 20:51:06 -0300

    NavBar
    Ajeitei um pouco da Barra de Navegação

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import Image from '../assets/honorlogo.png'
+import ImageLogo from '../assets/honorlogo.png'
+import PhoneIcon from "../assets/Phoneicon.png"
 
 const Navbar = () => {
   return (
     <nav className='bg-white border-gray-200 dark:bg=gray-900'>
-      <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-        <a href="#" className='flex items-center'> 
-          <img src={Image} alt="Honors Logo" className='h-8 mr-3'/>
+      <div className='flex flex-wrap items-center justify-between ml-[90px] mr-[74px]'>
+        <a href="#" className='flex items-center h-[85px] w-[344px]'> 
+          <img src={ImageLogo} alt="Honors Logo"/>
         </a>
+        <div className='flex flex-wrap items-center justify-between	h-[70px] w-[70px]'>
+          <a href="#">
+            <img src={PhoneIcon} alt="Phone Icon" classNama='w-[50px] h-[50px] flex-shrink'/>
+          </a>
+        </div>
       </div>
     </nav>
   )
