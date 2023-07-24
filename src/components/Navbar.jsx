ab35e9bc22ef9b0a96bd98627d4dbf2fe1327b36
CaioRasera <caiocremoninirasera@gmail.com>
2023-07-24 20:10:16 -0300

    Adicionei Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import Image from '../assets/honorlogo.png'
 
 const Navbar = () => {
   return (
-    <div className='text-transform: uppercase'>Navbar</div>
+    <nav className='bg-white border-gray-200 dark:bg=gray-900'>
+      <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+        <a href="#" className='flex items-center'> 
+          <img src={Image} alt="Honors Logo" className='h-8 mr-3'/>
+        </a>
+      </div>
+    </nav>
   )
 }
 
