ac44a06674e31d20efeeac332d9cab20562c0dba
Castrotech <107278965+Castrose@users.noreply.github.com>
2023-07-24 20:09:35 -0300

    MUDEI A LETRA

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='text-transform: uppercase'>Navbar</div>
+    <div className='text-transform: uppercase'>Navba</div>
   )
 }
 
