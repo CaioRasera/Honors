15584df65c15a4c185357fdec1a2f3f14e00f360
Castrotech <107278965+Castrose@users.noreply.github.com>
2023-07-25 12:17:26 -0300

    Adicionei um footer
    O footer não está adequadamente alinhado, mas estou ajustando para ficar mais alinhadinho

--- src/components/WhatWeDo.jsx
+++ src/components/WhatWeDo.jsx
@@ -2,24 +2,29 @@ import React from 'react'
 import Navbar from './Navbar'
 import Image from '../assets/honorsLogoWhite.png'
 import PhoneIcon from '../assets/phoneIconWhite.png'
+import NestJsImg from '../assets/NestJsImg.png'
+import ReactJsImg from '../assets/ReactJsImg.png'
+import TsImg from '../assets/TsImg.png'
+import JavaImg from '../assets/JavaImg.png'
 import '../assets/css/fonts.css'
 
 function WhatWeDo() {
     return (
         <div className='
             h-[100vh]
+            bg-[#202020]
         '>
             <div className="header flex justify-between ">
                 <a href="" className='mx-10 my-5'>
                     <img src={Image} alt="" />
                 </a>
-                <a href="" className='mx-10 my-5'>
+                <a href="" className='mx-10 my-5 w-11'>
                     <img src={PhoneIcon} alt="" />
                 </a>
             </div>
             <h1
                 className='
-                text-center font-extrabold text-5xl m-28
+                text-center font-extrabold text-5xl m-28 text-white
             '
                 style={{ fontFamily: 'poppins' }}
             >O que fazemos ?
@@ -43,11 +48,11 @@ function WhatWeDo() {
                     rounded-[12px]'/>
                     <div>
                         <h2 className='
-                            font-bold text-3xl leading-[100px] w-[100%]
+                            font-bold text-3xl leading-[100px] w-[100%] text-white
                         ' style={{ fontFamily: 'poppins' }}>
                             Do design ao código
                         </h2>
-                        <p style={{ fontFamily: 'poppins' }} className='text-center '>
+                        <p style={{ fontFamily: 'poppins' }} className='text-center text-white'>
                             Ajudamos você a construir uma aplicação com a sua cara de ponta a ponta
                         </p>
                     </div>
@@ -67,18 +72,41 @@ function WhatWeDo() {
 
                     <div>
                     <h2 className='
-                            font-bold text-3xl leading-[100px] w-[100%]
+                            font-bold text-3xl leading-[100px] w-[100%] text-white
                         ' style={{ fontFamily: 'poppins' }}>
                            Upgrade
                         </h2>
-                        <p style={{ fontFamily: 'poppins' }} className='text-center '>
+                        <p style={{ fontFamily: 'poppins' }} className='text-center text-white'>
                             Ajudamos você a construir uma aplicação com a sua cara de ponta a ponta
                         </p>
                     </div>
                 </div>
 
             </div>
-
+            <footer className=''>
+                <h3 className='
+                    text-white text-center font-[poppins] m-10
+                    text-l
+                '>
+                    Usando tecnologias modernas
+                </h3>
+                <div className='
+                    flex justify-center
+                '>
+                    <a href="" className='m-[35px'>
+                        <img src={NestJsImg} alt="" />
+                    </a>
+                    <a href="" className='m-[35px'>
+                        <img src={ReactJsImg} alt="" />
+                    </a>
+                    <a href="" className='m-[35px'>
+                        <img src={TsImg} alt="" />
+                    </a>
+                    <a href="" className='m-[35px'>
+                        <img src={JavaImg} alt="" />
+                    </a>
+                </div>
+            </footer>
         </div>
     )
 }
