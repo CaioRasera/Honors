17d3feaf246adc3e3ca6e924ee674326972cc902
Castrotech <107278965+Castrose@users.noreply.github.com>
2023-07-25 12:50:36 -0300

    "O que fazemos" quase concluído

--- src/components/WhatWeDo.jsx
+++ src/components/WhatWeDo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
-import Image from '../assets/honorsLogoWhite.png'
+import HonorsLogo from '../assets/honorsLogoWhite.png'
 import PhoneIcon from '../assets/phoneIconWhite.png'
 import NestJsImg from '../assets/NestJsImg.png'
 import ReactJsImg from '../assets/ReactJsImg.png'
@@ -10,30 +10,49 @@ import '../assets/css/fonts.css'
 
 function WhatWeDo() {
     return (
-        <div className='
+        <main className='
             h-[100vh]
+            w-[100vw]
             bg-[#202020]
         '>
-            <div className="header flex justify-between ">
-                <a href="" className='mx-10 my-5'>
-                    <img src={Image} alt="" />
+            <div className="
+            header 
+            flex 
+            justify-between 
+            ">
+                <a href="" className='
+                mx-10 
+                my-5 
+                w-28'>
+                    <img src={HonorsLogo} alt="" />
                 </a>
-                <a href="" className='mx-10 my-5 w-11'>
+                <a href="" className='
+                mx-10 
+                my-5 
+                w-7'>
                     <img src={PhoneIcon} alt="" />
                 </a>
             </div>
             <h1
                 className='
-                text-center font-extrabold text-5xl m-28 text-white
+                text-center 
+                font-extrabold 
+                text-xl 
+                m-20 
+                md:text-2xl 
+                text-white
             '
                 style={{ fontFamily: 'poppins' }}
             >O que fazemos ?
             </h1>
             <div className='
-            flex 
+            md:flex 
             justify-evenly 
-            my-20 
+            m-35 
+            mb-24
             text-center
+            grid
+            grid-rows
             '>
                 <div className='
                     w-[350px]
@@ -44,15 +63,18 @@ function WhatWeDo() {
                     border-[#38108C]
                     border-2
                     inline-block
-                    w-[228.9px]
+                    w-[79px]
+                    md:w-[228.9px]
                     rounded-[12px]'/>
                     <div>
                         <h2 className='
-                            font-bold text-3xl leading-[100px] w-[100%] text-white
+                            font-bold text-[14px] leading-[100px] w-[100%] text-white
+                            md:text-3xl
+
                         ' style={{ fontFamily: 'poppins' }}>
                             Do design ao código
                         </h2>
-                        <p style={{ fontFamily: 'poppins' }} className='text-center text-white'>
+                        <p style={{ fontFamily: 'poppins' }} className='text-center text-[12px] w-[75%]  m-auto text-white'>
                             Ajudamos você a construir uma aplicação com a sua cara de ponta a ponta
                         </p>
                     </div>
@@ -67,47 +89,48 @@ function WhatWeDo() {
                     border-[#38108C]
                     border-2
                     inline-block
-                    w-[228.9px]
+                    w-[79px]
+                    md:w-[228.9px]
                     rounded-[12px]'/>
 
                     <div>
                     <h2 className='
-                            font-bold text-3xl leading-[100px] w-[100%] text-white
+                            font-bold text-[14px] leading-[100px] w-[100%] text-white
+                            md:text-3xl
                         ' style={{ fontFamily: 'poppins' }}>
                            Upgrade
                         </h2>
-                        <p style={{ fontFamily: 'poppins' }} className='text-center text-white'>
+                        <p style={{ fontFamily: 'poppins' }} className='text-center text-[12px] w-[75%] m-auto text-white'>
                             Ajudamos você a construir uma aplicação com a sua cara de ponta a ponta
                         </p>
                     </div>
                 </div>
 
             </div>
-            <footer className=''>
+            <div className='m-7'>
                 <h3 className='
-                    text-white text-center font-[poppins] m-10
-                    text-l
+                    text-white text-center text-[12px]  font-[poppins]
                 '>
                     Usando tecnologias modernas
                 </h3>
                 <div className='
                     flex justify-center
                 '>
-                    <a href="" className='m-[35px'>
+                    <a href="https://nestjs.com/" className='m-[35px'>
                         <img src={NestJsImg} alt="" />
                     </a>
-                    <a href="" className='m-[35px'>
+                    <a href="https://react.dev/" className='m-[35px'>
                         <img src={ReactJsImg} alt="" />
                     </a>
-                    <a href="" className='m-[35px'>
+                    <a href="https://www.typescriptlang.org/" className='m-[35px'>
                         <img src={TsImg} alt="" />
                     </a>
-                    <a href="" className='m-[35px'>
+                    <a href="https://www.java.com/pt-BR/" className='m-[35px'>
                         <img src={JavaImg} alt="" />
                     </a>
                 </div>
-            </footer>
-        </div>
+            </div>
+        </main>
     )
 }
 
